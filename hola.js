b0460321fe7cd0e7537d77e942222d94290f63d2
“Diegohola” <dekidsmx@gmail.com>
2015-12-03 17:56:06 -0600

    1era version

---
+++ hola.js
@@ -0,0 +1,5 @@
+  var options = [
+    {selector: '.class', offset: 200, callback: 'globalFunction()' },
+    {selector: '.other-class', offset: 200, callback: 'globalFunction()' },
+  ];
+  Materialize.scrollFire(options);
---
+++ js/init.js
@@ -0,0 +1,14 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+
+  }); // end of document ready
+})(jQuery); 
+
+ $(document).ready(function(){
+      $('.slider').slider({full_width: true});
+    });// end of jQuery name space
+
+
+        
